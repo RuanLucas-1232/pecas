40b37b8f7c2e0d433b287e22b2edcfe34f2f39d3
RuanLucas-1232 <ruanlucas1232@gmail.com>
2021-08-24 17:33:01 -0300

    Adicionando sistema de cadastro de peças

---
+++ sistema-de-pecas.js
@@ -0,0 +1,21 @@
+// Lista de Peças
+let pecas = ["Motor","radiador","Para-brisas"]
+// Verificação se a lista está completa
+if (pecas.length < 10) {
+    console.log("Ólá! Castre sua peça.","Cadastre uma peça superio a 100 g e que tenha, no minimo, 4 letras");
+    let pecasDoUsuario = "Pneus"
+    let pesoDaPeca = "120 g"
+    // Verificação se a peça pesa 100 g
+    if (pesoDaPeca > "100 g") {
+        // Verificação se a peça tem, no minimo, quatro letras
+        if (pecasDoUsuario.length >= 4) {
+            console.log("A peça foi cadastrada");
+        } else {
+            console.log("A peça não pode ser cadastrada, pois tem menos de quatro letras");
+        }
+    } else {
+        console.log("A peça não pode ser cadastrada, pois seu peso não é superior a 100 g");
+    }
+} else {
+    console.log("Desculpa! Atingiu o limite de peças");
+}
